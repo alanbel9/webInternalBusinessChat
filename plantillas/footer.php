0c1d5e613a93443b7af1321987d732c335a2abf5
Hector <hectorbg303@gmail.com>
2019-08-07 23:33:47 +0200

    cambios Nav

--- plantillas/footer.php
+++ plantillas/footer.php
@@ -1,4 +1,13 @@
 <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+    $(function(){
+        $(".barraIzquierda").children().on("click", function(evt){
+            $(this).next().slideToggle();
+        });
+    })
+</script>
+
 </body>
 </html>
