215575f501e8b5acbc0b7fd0a8379f7057f1a34e
Hector <hectorbg303@gmail.com>
2019-08-08 12:21:02 +0200

    iconos

--- plantillas/nav.php
+++ plantillas/nav.php
@@ -1,10 +1,10 @@
 <div class="row">
-  <div class="col" style="white-space: nowrap; color:black !important">
+  <div class="col-md-2" style="white-space: nowrap; color:black !important">
     <div class="nav flex-column nav-pills mt-5 barraIzquierda" id="v-pills-tab" role="tablist"
       aria-orientation="vertical" style="width:fit-content">
       <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
         aria-controls="v-pills-home" aria-selected="true">
-        <h4>Grupo amigos</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Grupo amigos</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -17,7 +17,7 @@
       </div>
       <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
         aria-controls="v-pills-profile" aria-selected="false">
-        <h4>Café</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Café</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -30,7 +30,7 @@
       </div>
       <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
         aria-controls="v-pills-messages" aria-selected="false">
-        <h4>Futbolín</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Futbolín</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -43,7 +43,7 @@
       </div>
       <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
         aria-controls="v-pills-settings" aria-selected="false">
-        <h4>Ping-pong</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Ping-pong</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -56,7 +56,7 @@
       </div>
       <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
         aria-controls="v-pills-settings" aria-selected="false">
-        <h4>Fumar</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Fumar</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -69,7 +69,7 @@
       </div>
       <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
         aria-controls="v-pills-settings" aria-selected="false">
-        <h4>PHP</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>PHP</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -82,7 +82,7 @@
       </div>
       <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
         aria-controls="v-pills-settings" aria-selected="false">
-        <h4>Java</h4>
+        <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Java</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
         style="display: none;">
@@ -95,7 +95,7 @@
       </div>
     </div>
   </div>
-  <div class="col-9" style="border-style: dotted;">
+  <div class="col-md-10" style="border-style: dotted;">
     Pantalla chat
   </div>
 </div>
