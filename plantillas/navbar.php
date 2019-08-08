c2e868b6a781596cef649a6a1dcb503b4c6378ae
Alán <49872323+alanbel9@users.noreply.github.com>
2019-08-08 12:40:39 +0200

    navbar con iconos fontawesome

--- plantillas/navbar.php
+++ plantillas/navbar.php
@@ -1,16 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="../html/index.php"><h1><i class="fa fa-users" aria-hidden="true"></i> Chat;)</h1></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="../html/index.php">
+        <h1>
+            <i class="fa fa-users" aria-hidden="true"></i> Chat;)
+        </h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
         <form class="form-inline my-2 my-lg-0 m-auto">
-            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-            <button class="btn btn-info my-2 my-sm-0" type="submit">Buscar</button>
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-info my-2 my-sm-0" type="submit">
+                        <h6>  <i class="fa fa-search" aria-hidden="true"></i>  Buscar  </h6>
+                </button>
         </form>
-        <a class="button btn btn-success my-2 my-sm-0 mr-sm-2" href="../html/modificarPerfil.php">Perfil</a>
-        <button class="btn btn-danger my-2 my-sm-0" type="submit">Logout</button>
+        
+        <a class="button btn btn-success my-2 my-sm-0 mr-sm-2" href="../html/modificarPerfil.php">
+                <h6> <i class="fa fa-cog" aria-hidden="true"></i>  Perfil   </h6>
+        </a>
+        
+        <button class="btn btn-danger my-2 my-sm-0" type="submit">
+                <h6> <i class="fa fa-sign-out " aria-hidden="true"></i>  Logout    </h6>
+        </button>
     </div>
 </nav>
