a3d4594852c034b30b37e240eca8cd63d50c18e3
Hector <hectorbg303@gmail.com>
2019-08-08 11:56:05 +0200

    Update navbar.php

--- plantillas/navbar.php
+++ plantillas/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="../html/index.php"><h1>Chat</h1></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,9 +8,9 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <form class="form-inline my-2 my-lg-0 m-auto">
             <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Buscar</button>
+            <button class="btn btn-info my-2 my-sm-0" type="submit">Buscar</button>
         </form>
-        <a class="button btn btn-outline-success my-2 my-sm-0 mr-sm-2" href="../html/modificarPerfil.php">Perfil</a>
-        <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Logout</button>
+        <a class="button btn btn-success my-2 my-sm-0 mr-sm-2" href="../html/modificarPerfil.php">Perfil</a>
+        <button class="btn btn-danger my-2 my-sm-0" type="submit">Logout</button>
     </div>
 </nav>
