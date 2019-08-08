b0738d20798a6835005ce30421864a58c258111a
Hector <hectorbg303@gmail.com>
2019-08-08 12:31:00 +0200

    iconos

--- plantillas/navbar.php
+++ plantillas/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="../html/index.php"><h1>Chat</h1></a>
+    <a class="navbar-brand" href="../html/index.php"><h1><i class="fa fa-users" aria-hidden="true"></i> Chat;)</h1></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
