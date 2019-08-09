db29a7c05f1e4ac494e19bbb437b57b66751790d
Alán <49872323+alanbel9@users.noreply.github.com>
2019-08-09 09:07:49 +0200

    Update nav.php

--- plantillas/nav.php
+++ plantillas/nav.php
@@ -5,57 +5,63 @@
                       <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Grupo amigos</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+
                   <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">
                         <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Café</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+
                   <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
                          <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Futbolín</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+
                   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
                           <h4>Ping-pong</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"  style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-home" role="tab"  aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab"  aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+
                   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
                         <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Fumar</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+
                   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
                          <h4><i class="fa fa-hashtag" aria-hidden="true"></i>PHP</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5"  data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
+                  
                   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"  aria-controls="v-pills-settings" aria-selected="false">
                     <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Java</h4>
                   </a>
                   <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="display: none;">
-                          <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
+                          <a id="v-pills-home-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
                           <a id="recarga" class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Información</a>
-                          <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
+                          <a id="v-pills-profile-tab" class="ml-5 mr-5" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
                   </div>
           </div>
     </div>
