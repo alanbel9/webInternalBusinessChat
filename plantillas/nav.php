8f7e0d277e2d0815ea811d0887f271f54e60e3b4
Alán <49872323+alanbel9@users.noreply.github.com>
2019-08-09 08:50:54 +0200

    modif ajax

--- plantillas/nav.php
+++ plantillas/nav.php
@@ -1,9 +1,7 @@
 <div class="row">
-  <div class="col-md-2" style="white-space: nowrap; color:black !important">
-    <div class="nav flex-column nav-pills mt-5 barraIzquierda" id="v-pills-tab" role="tablist"
-      aria-orientation="vertical" style="width:fit-content">
-      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
-        aria-controls="v-pills-home" aria-selected="true">
+      <div class="col-md-2" style="white-space: nowrap; color:black !important">
+          <div class="nav flex-column nav-pills mt-5 barraIzquierda" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="width:fit-content">
+              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
         <h4><i class="fa fa-hashtag" aria-hidden="true"></i>Grupo amigos</h4>
       </a>
       <div class="nav flex-column nav-pills emergente" id="v-pills-tab" role="tablist" aria-orientation="vertical"
@@ -88,22 +86,21 @@
         style="display: none;">
         <a class="ml-5 mr-5" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
           aria-controls="v-pills-home" aria-selected="true">Acceder al chat</a>
-        <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
+
+        <a id="recarga" class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
           aria-controls="v-pills-profile" aria-selected="false">Información</a>
         <a class="ml-5 mr-5" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
           aria-controls="v-pills-profile" aria-selected="false">Opciones</a>
       </div>
     </div>
   </div>
-  <div class="col-md-10 overflow-auto " style="border-style: dotted;  ">
+
+ 
+
+  <div id="modificado"   class="col-md-10 overflow-auto " style="border-style: dotted;  ">
     
-      
-
-
-  <br>
-
-
-
 
   </div>
+
+  
 </div>
