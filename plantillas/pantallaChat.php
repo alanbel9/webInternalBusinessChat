7b7971ca1e5d78ff47ccdad6b0d6f7e2dc7e7798
Hector <hectorbg303@gmail.com>
2019-08-14 18:16:20 +0200

    Update pantallaChat.php

--- plantillas/pantallaChat.php
+++ plantillas/pantallaChat.php
@@ -21,7 +21,7 @@
     </blockquote>
     <blockquote class="blockquote pt-3">
         <div class="row">
-            <div class="col-4"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+            <div class="col-4"><i class="fa fa-envelope-open" aria-hidden="true"></i><span class="font-italic text-info">
                     Nombre</span></div>
             <div class="col-8 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
                     class="font-italic text-warning text-nowrap"> 00:00:00 (1-01-2019) </span></div>
@@ -34,7 +34,7 @@
     </blockquote>
     <blockquote class="blockquote pt-3">
         <div class="row">
-            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+            <div class="col-6"><i class="fa fa-envelope" aria-hidden="true"></i><span class="font-italic text-info">
                     Nombre</span></div>
             <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
                     class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
@@ -47,7 +47,7 @@
     </blockquote>
     <blockquote class="blockquote pt-3">
         <div class="row">
-            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+            <div class="col-6"><i class="fa fa-envelope-open-o" aria-hidden="true"></i><span class="font-italic text-info">
                     Nombre</span></div>
             <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
                     class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
