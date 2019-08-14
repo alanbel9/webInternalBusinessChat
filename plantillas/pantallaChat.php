c4434a188d285a17de95b68a26e9c36a96d04acc
Hector <hectorbg303@gmail.com>
2019-08-14 18:03:02 +0200

    Update pantallaChat.php

--- plantillas/pantallaChat.php
+++ plantillas/pantallaChat.php
@@ -1,9 +1,17 @@
-<div id="pantallaConversaciones" class="container bg-light text-dark shadow " style="font-family: 'Righteous', sans-serif;">
-    <br/>
+<div id="pantallaConversaciones" class="container bg-light text-dark shadow"
+    style="font-family: 'Righteous', sans-serif;">
+    <br />
     <h1>
         Pantalla de conversaciones
     </h1>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
+
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam similique exercitationem earum dolore
             excepturi,
@@ -11,21 +19,39 @@
             assumenda?
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-open-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed ratione molestiae perspiciatis eligendi
             totam fugiat animi. Molestias autem expedita eius error nihil praesentium porro id dolore dignissimos?
             Harum, iure.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet qui nostrum ipsam fugit quis, laborum
             quibusdam facere dignissimos cum ducimus debitis adipisci, blanditiis earum asperiores similique suscipit
             quae soluta.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam similique exercitationem earum dolore
             excepturi,
@@ -33,21 +59,39 @@
             assumenda?
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-open" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed ratione molestiae perspiciatis eligendi
             totam fugiat animi. Molestias autem expedita eius error nihil praesentium porro id dolore dignissimos?
             Harum, iure.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet qui nostrum ipsam fugit quis, laborum
             quibusdam facere dignissimos cum ducimus debitis adipisci, blanditiis earum asperiores similique suscipit
             quae soluta.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam similique exercitationem earum dolore
             excepturi,
@@ -55,21 +99,39 @@
             assumenda?
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-open-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed ratione molestiae perspiciatis eligendi
             totam fugiat animi. Molestias autem expedita eius error nihil praesentium porro id dolore dignissimos?
             Harum, iure.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet qui nostrum ipsam fugit quis, laborum
             quibusdam facere dignissimos cum ducimus debitis adipisci, blanditiis earum asperiores similique suscipit
             quae soluta.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam similique exercitationem earum dolore
             excepturi,
@@ -77,14 +139,26 @@
             assumenda?
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope-open" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed ratione molestiae perspiciatis eligendi
             totam fugiat animi. Molestias autem expedita eius error nihil praesentium porro id dolore dignissimos?
             Harum, iure.
         </p>
     </blockquote>
-    <blockquote class="blockquote text-left"><i class="fa fa-envelope" aria-hidden="true"></i>
+    <blockquote class="blockquote pt-3">
+        <div class="row">
+            <div class="col-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="font-italic text-info">
+                    Nombre</span></div>
+            <div class="col-6 text-right"><i class="fa fa-clock-o" aria-hidden="true"></i><span
+                    class="font-italic text-warning"> 00:00:00 (1-01-2019) </span></div>
+        </div>
         <p class="mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet qui nostrum ipsam fugit quis, laborum
             quibusdam facere dignissimos cum ducimus debitis adipisci, blanditiis earum asperiores similique suscipit
@@ -98,11 +172,13 @@
     <div class='row align-items-center'>
         <div class='col-md-1'></div>
         <div class='col-md-9'>
-            <textarea id="mensajeEnviar" type="text" class="form-control w-100 mb-1" id="mensaje" placeholder="Mensaje" rows="2"></textarea>
+            <textarea id="mensajeEnviar" type="text" class="form-control w-100 mb-1" id="mensaje" placeholder="Mensaje"
+                rows="2"></textarea>
         </div>
         <div class='col-md-1 text-center'>
-            <button id="botonEnviar" class="btn btn-primary"><h6 class="m-2"><i class="fa fa-envelope-o"
-                    aria-hidden="true"></i> Enviar</h6></button>
+            <button id="botonEnviar" class="btn btn-primary">
+                <h6 class="m-2"><i class="fa fa-envelope-o" aria-hidden="true"></i> Enviar</h6>
+            </button>
         </div>
         <div class='col-md-1'></div>
     </div>
