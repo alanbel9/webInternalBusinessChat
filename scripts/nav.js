780748139414b9475e6820c8b51839d931cc2409
Hector <hectorbg303@gmail.com>
2019-08-15 13:42:22 +0200

    Update nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,16 +3,22 @@ $(function () {
         $(".barraIzquierda").children("a").next().slideUp();
         $(this).next().slideDown();
 
-        $(".contenedorPantallas").hide().load("../plantillas/pantallaChat.php").fadeIn('500');
+        $(".contenedorPantallas").fadeOut(500,function(){
+            $(this).load("../plantillas/pantallaChat.php").slideDown(1000);
+        })
     })
     // recarga mediante AJAX del index.html al hacer clic en los grupos
     // 1 función para cada enlace ????
     $(".infoGrupo").click(function (event) {
-        $(".contenedorPantallas").hide().load('../plantillas/pantallaInfoGrupo.php').fadeIn('500');   // grupo java
+        $(".contenedorPantallas").fadeOut(500,function(){
+            $(this).load('../plantillas/pantallaInfoGrupo.php').fadeIn(500);   // grupo java
+        });
     });
 
     $(".botonBuscar").click(function (event) {
-        $(".contenedorPantallas").hide().load('../plantillas/pantallaBuscar.php').fadeIn('500');   // grupo java
+        $(".contenedorPantallas").fadeOut(500,function(){
+            $(this).load('../plantillas/pantallaBuscar.php').fadeIn(500);   // grupo java
+        });
     });
 })
 
