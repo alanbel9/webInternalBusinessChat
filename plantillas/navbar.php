bb722a349fa17df8c4a18cdb618551337ba2a2e9
Hector <hectorbg303@gmail.com>
2019-08-15 11:57:14 +0200

    Seleccion grupos

--- plantillas/navbar.php
+++ plantillas/navbar.php
@@ -8,6 +8,25 @@
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="dropright">
+            <button class="btn btn-outline-secondary dropdown-toggle ml-5" type="button" id="dropdownMenuButton"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Grupos de Chat;)
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Grupo 1 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 2 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 3 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 4 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 5 nuevo</a>
+                <a class="dropdown-item disabled " href="#">Grupo 6</a>
+                <a class="dropdown-item" href="#">Grupo 7 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 8 nuevo</a>
+                <a class="dropdown-item disabled " href="#">Grupo 9</a>
+                <a class="dropdown-item" href="#">Grupo 10 nuevo</a>
+                <a class="dropdown-item" href="#">Grupo 11 nuevo</a>
+            </div>
+        </div>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
@@ -17,7 +36,7 @@
                 <h6 class="mt-2"> <i class="fa fa-search" aria-hidden="true"></i> Buscar </h6>
             </button>
         </form>
-
+        
         <a class="button btn btn-success my-2 my-sm-0 mr-sm-2" href="../html/modificarPerfil.php">
             <h6 class="mt-2"> <i class="fa fa-cog" aria-hidden="true"></i> Perfil </h6>
         </a>
