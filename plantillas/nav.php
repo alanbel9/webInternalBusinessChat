78e3e1e36544322c9060dd97d892f2fe00af79d8
Hector <hectorbg303@gmail.com>
2019-08-15 19:06:24 +0200

    Update nav.php

--- plantillas/nav.php
+++ plantillas/nav.php
@@ -98,18 +98,13 @@
 <div id="modalInfoUsuario" class="modal fade" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
       <div class="modal-body">
       <?php
           require("../plantillas/pantallaPerfil.php");
       ?>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
       </div>
     </div>
   </div>
