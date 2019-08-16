1cb295a32784a0ef191bd43c46b17212036e6ac3
Hector <hectorbg303@gmail.com>
2019-08-16 16:38:11 +0200

    Update pantallaPerfil.php

--- plantillas/pantallaPerfil.php
+++ plantillas/pantallaPerfil.php
@@ -1,15 +1,15 @@
 <div class="container bg-light text-dark shadow p-2"
     style="font-family: 'Righteous', sans-serif;min-height:90vh;">
-    <div class="row pt-5">
+    <div class="row pt-4">
         <div class="mx-auto">
             <img id="imgSalida" height="300" src="../resources/foto-carnet.jpg" class="border border-dark"
                 alt="foto perfil">
         </div>
     </div>
 
-    <h1 class="pt-5" style="text-align: center">Mi_Nombre Apellido1 Apellido2</h1>
+    <h1 class="pt-4" style="text-align: center">Mi_Nombre Apellido1 Apellido2</h1>
 
-    <h3 class="pt-5">Mis datos personales:</h3>
+    <h3 class="pt-4">Mis datos personales:</h3>
 
     <blockquote><strong>Fecha de nacimiento:</strong> dd/mm/aaaa</blockquote>
     <blockquote><strong>Correo:</strong> correoDePrueba@gmail.com</blockquote>
