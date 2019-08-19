493e52d2f14ef203a88e9d187dd9fc740f0b401e
Hector <hectorbg303@gmail.com>
2019-08-19 19:50:01 +0200

    Update pantallaModificarPerfil.php

--- plantillas/pantallas/pantallaModificarPerfil.php
+++ plantillas/pantallas/pantallaModificarPerfil.php
@@ -117,30 +117,3 @@
         
         
 </form>
-<script type="text/javascript">
-    
-    $(window).load(function(){
-        $(function() {
-            $('#file-input').change(function(e) {
-                addImage(e); 
-            });
-
-            function addImage(e){
-                var file = e.target.files[0],
-                imageType = /image.*/;
-                if (!file.type.match(imageType))
-                return;
-
-                var reader = new FileReader();
-                reader.onload = fileOnload;
-                reader.readAsDataURL(file);
-            }
-
-            function fileOnload(e) {
-                var result=e.target.result;
-                $('#imgSalida').attr("src",result);
-            }
-        });
- });
-
-</script>
