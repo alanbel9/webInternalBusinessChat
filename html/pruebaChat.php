0fd031f5a56015621500417b05edeaa6a1f50de5
alejandro <alejandro@mail.com>
2019-08-20 08:13:41 +0200

    ideasChat

---
+++ html/pruebaChat.php
@@ -0,0 +1,99 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<div class="container mt-5">
+
+  <!-- Grid row -->
+  <div class="row d-flex flex-row-reverse">
+
+    <!-- Grid column -->
+    <div class="col-md-6 mb-4 d-flex flex-row-reverse">
+
+      <div class="card chat-room small-chat wide" id="myForm">
+        <div class="card-header white d-flex justify-content-between p-2" id="toggle" style="cursor: pointer;">
+          <div class="heading d-flex justify-content-start">
+            <div class="profile-photo">
+              
+              <span class="state"></span>
+            </div>
+           
+            <div class="data">
+              <p class="name mb-0"><strong>Alex M</strong></p>
+              <p class="activity text-muted mb-0">En linea</p>
+            </div>
+          </div>
+          <div class="icons grey-text">
+            <a class="feature"><i class="fas fa-video mr-2"></i></a>
+            <a class="feature"><i class="fas fa-phone mr-2"></i></a>
+            <a class="feature"><i class="fas fa-cog mr-2"></i></a>
+            <a id="closeButton"><i class="fas fa-times mr-2"></i></a>
+          </div>
+        </div>
+        <div class="my-custom-scrollbar" id="message">
+          <div class="card-body p-3">
+            <div class="chat-message">
+              <div class="media mb-3">
+               
+               
+              </div>
+              <div class="card bg-secondary rounded w-75 float-right z-depth-0 mb-1">
+                <div class="card-body p-2">
+                  <p class="card-text text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit voluptatem cum eum tempore.</p>
+                  <span class="time-right">11:00</span>
+                </div>
+              </div>
+              <div class="card bg-secondary rounded w-50 float-right z-depth-0 mb-2">
+                <div class="card-body p-2">
+                  <p class="card-text text-white">Rem suscipit lorum repellendus ditiis?</p>
+                  <span class="time-left">11:01</span>
+                </div>
+              </div>
+              <div class="card bg-success rounded w-75 z-depth-0 mb-1 message-text">
+                <div class="card-body p-2">
+                  <p class="card-text text-white">Nostrum minima cupiditate assumenda, atque cumque hic voluptatibus at corporis maxime quam harum.</p>
+                  <span class="time-right">11:02</span>
+                </div>
+              </div>
+              <div class="d-flex justify-content-start">
+                <div class="profile-photo message-photo">
+                  
+                  <span class="state"></span>
+                </div>
+                <div class="card bg-success rounded w-75 z-depth-0 mb-2">
+                  <div class="card-body p-2">
+                    <p class="card-text text-white">Qui animi molestiae autem nihil optio recusandae nisi sit ab quo est.</p>
+                    <span class="time-left">11:05</span>
+                  </div>
+                </div>
+              </div>
+              <div class="card bg-secondary rounded w-75 float-right z-depth-0 mb-1 last">
+                <div class="card-body p-2">
+                  <p class="card-text text-white">Maxime nostrum ut blanditiis a quod quam, quidem deleniti?</p>
+                  <span class="time-right">11:07</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted white pt-1 pb-2 px-3">
+          <input type="text" id="exampleForm2" class="form-control" placeholder="Escribe..."> 
+          <button type="button" class="btn btn-dark ml-auto">Enviar</button>
+         
+          <div>
+            <a><i class="far fa-file-image mr-2"></i></a>
+            <a><i class="far fa-laugh mr-2"></i></a>
+            <a><i class="fas fa-gamepad mr-2"></i></a>
+            <a><i class="fas fa-paperclip mr-2"></i></a>
+            <a><i class="fas fa-camera mr-2"></i></a>
+            <a><i class="fas fa-thumbs-up float-right"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
