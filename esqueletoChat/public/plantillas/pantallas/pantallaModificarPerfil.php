3289dcd24b7dc00cf899ef3b4f9a1bc6392b69bf
Alán <49872323+alanbel9@users.noreply.github.com>
2019-08-21 11:48:54 +0200

    añado todo a carpeta public

---
+++ esqueletoChat/public/plantillas/pantallas/pantallaModificarPerfil.php
@@ -0,0 +1,154 @@
+
+
+<br>
+
+<div class="">  
+       
+       <div class="row">
+               <div class=" rounded mx-auto d-block">
+                       <img id="imgSalida" width="" height="300" src="../resources/foto-carnet.jpg" class="rounded" alt="foto perfil">
+                       <!--  URL de las fotos asi:   ../resources/foto-carnet.jpg   -->
+               </div>
+       </div>
+   
+       <div class="row">
+                <div class="col-md-4 offset-md-3 mt-2 ">
+                           <div class="custom-file">
+                                   <input id="fotoUsuario" name="foto" type="file" class="custom-file-input" aria-describedby="inputGroupFileAddon03" required>
+                                   <label class="custom-file-label" for="inputGroupFile03">Choose file</label>
+                           </div>
+                </div>
+
+                <div class="col-md-2  mt-2">
+                       <button class="btn btn-primary btn-block " type=""> Subir imagen </button>
+                </div>              
+        </div>
+   
+</div> 
+
+<br>
+<div class="dropdown-divider"></div>
+<br>
+
+<div class="">  
+       <div class="row">
+               <div class="input-group  col-md-3 offset-md-3 mt-2">
+                       <div class="input-group-prepend">
+                           <span class="input-group-text" id="basic-addon3">Nombre</span>
+                       </div>
+                       <input id="nombreUsuario" type="text" name="nombre" class="form-control" aria-describedby="basic-addon3" required>
+               </div>
+   
+               <div class="input-group col-md-3 mt-2">
+                       <div class="input-group-prepend">
+                               <span class="input-group-text" >Apellidos</span>
+                           </div>
+                       <input id="apellidosUsuario" type="text" name="apellidos" class="form-control" required>
+               </div>
+   
+       </div>
+
+       <div class="row">
+               <div class="input-group  col-md-3 offset-md-3 mt-2">
+                        <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon3">Fecha nac.</span>
+                        </div>
+                        <input id="nacimientoUsuario" type="date" name="nacimiento" class="form-control" aria-describedby="basic-addon3"  required>
+             </div>
+   
+               <div class="input-group col-md-3 mt-2  ">
+               <div class="input-group-prepend">
+                                   <span class="input-group-text" id="basic-addon3">Puesto</span>
+                           </div>
+                           <select id="puestoUsuario" name="puesto" class="custom-select"  required >
+                                   <option selected> </option>
+                                   <option value="desarrollador">Desarrollador</option>
+                                   <option value="analista">Analista</option>
+                                   <option value="sistemas">Sistemas</option>
+                                   <option value="seguridad">Seguridad</option>
+                                   <option value="recursos humanos">Recursos Humanos</option>
+                                   <option value="administracion">Administración</option>
+                           </select>
+               </div>
+   
+       </div>
+   
+   
+       <div class="row">
+               <div class=" input-group col-md-6 mt-2 offset-md-3 ">
+                       <div class="input-group-prepend">
+                               <span class="input-group-text" id="basic-addon3">Correo</span>
+                       </div>
+                       <input id="inputCorreo" type="text" name="correo" class="form-control" readonly >
+               </div>
+       </div>
+   
+           <div class="row">
+                   <div class="col-md-3 offset-md-3 mt-2">
+                       <input type="password" name="password1" class="form-control" placeholder="Password" required>
+                   </div>
+                   
+                   <div class="col-md-3 mt-2  ">
+                       <input type="password" name="password2" class="form-control" placeholder="Confirm password" required>
+                   </div>    
+           </div>
+   
+   
+           <div class="row">            
+                   <div class="input-group col-md-6 mt-2 offset-md-3">
+                           <div class="input-group-prepend">
+                               <span class="input-group-text">Conocimientos</span>
+                           </div>
+                           <textarea id="conocimientosUsuario" name="conocimientos" class="form-control" aria-label="With textarea" required></textarea>
+                   </div>
+                   <div class="col-md-2"></div>
+           </div>
+   
+           <div class="row">            
+   
+                   <div class="input-group col-md-6 mt-2 offset-md-3">
+                           <div class="input-group-prepend">
+                               <span class="input-group-text">Aficiones</span>
+                           </div>
+                           <textarea id="aficionesUsuario" name="aficiones" class="form-control" aria-label="With textarea" required></textarea>
+                   </div>
+                   <div class="col-md-2"></div>
+           </div>
+    
+           <div class="row">
+                   <div class="col-md-6 offset-md-3 mt-2">
+                       <button class="btn btn-primary btn-block p-2" type=""> Confirmar </button>
+                   </div>              
+           </div>
+           
+   </div>
+   
+<br><br>
+
+<script type="text/javascript">
+        $(window).on('load',function(){
+        //$(window).load(function(){
+        $(function() {
+            $('#fotoUsuario').change(function(e) {
+                addImage(e); 
+            });
+
+            function addImage(e){
+                var file = e.target.files[0],
+                imageType = /image.*/;
+                if (!file.type.match(imageType))
+                return;
+
+                var reader = new FileReader();
+                reader.onload = fileOnload;
+                reader.readAsDataURL(file);
+            }
+
+            function fileOnload(e) {
+                var result=e.target.result;
+                $('#imgSalida').attr("src",result);
+            }
+        });
+ });
+
+</script>
---
+++ esqueletoChat/public/plantillas/pantallas/pantallaPerfil.php
@@ -0,0 +1,29 @@
+<div class="container bg-light text-dark shadow p-2"
+    style="font-family: 'Righteous', sans-serif;min-height:90vh;">
+    <div class="row pt-4">
+        <div class="mx-auto">
+            <img id="imgSalida" height="300" src="../resources/foto-carnet.jpg" class="border border-dark"
+                alt="foto perfil">
+        </div>
+    </div>
+
+    <h1 class="pt-4" style="text-align: center">Mi_Nombre Apellido1 Apellido2</h1>
+
+    <h3 class="pt-4">Mis datos personales:</h3>
+
+    <blockquote><strong>Fecha de nacimiento:</strong> dd/mm/aaaa</blockquote>
+    <blockquote><strong>Correo:</strong> correoDePrueba@gmail.com</blockquote>
+    <blockquote><strong>Puesto de trabajo:</strong> php</blockquote>
+    <blockquote><strong>Conocimientos:</strong>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iure sed facilis, corporis unde officia quod
+            odit optio nemo quas, inventore non possimus provident illum tempora molestias at quis ut.</p>
+    </blockquote>
+    <blockquote><strong>Aficiones:</strong>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis autem earum quidem, vel consequatur iure
+            alias maiores nisi? Omnis quas fuga eum error itaque quasi incidunt quia distinctio similique ex.</p>
+    </blockquote>
+    <blockquote><strong>Grupos de chat:</strong>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis autem earum quidem, vel consequatur iure
+            alias maiores nisi? Omnis quas fuga eum error itaque quasi incidunt quia distinctio similique ex.</p>
+    </blockquote>
+</div>
